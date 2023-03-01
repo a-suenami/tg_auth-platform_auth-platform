1b3abe259b929b4d46ad4a168d9cba960f46a6be
kazuma murata <k.murata@twogate.com>
2023-03-01 10:36:58 +0000

    tapioca dsl

--- db/schema.rb
+++ db/schema.rb
@@ -12,14 +12,6 @@
 
 ActiveRecord::Schema[7.0].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "citext"
-  enable_extension "pgcrypto"
   enable_extension "plpgsql"
-
-  create_table "tenants", id: :string, force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
