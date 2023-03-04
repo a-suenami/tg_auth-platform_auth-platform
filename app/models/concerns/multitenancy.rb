de65b9b7e9231a1d043ddc9b558838e2cd24e737
keiya <keiya_21@yahoo.co.jp>
2023-03-04 11:26:14 +0900

    add type

--- app/models/concerns/multitenancy.rb
+++ app/models/concerns/multitenancy.rb
@@ -1,10 +1,13 @@
-# typed: false
+# typed: strict
 
 module Multitenancy
   extend ActiveSupport::Concern
 
   module ClassMethods
+    extend T::Sig
+    sig { returns(T.nilable(ActiveRecord::Relation)) }
     def default_scope
+      T.bind(self, ActiveRecord::Querying)
       where(tenant_id: RequestStore.store[:current_tenant]) if RequestStore.store[:current_tenant].present?
     end
   end
