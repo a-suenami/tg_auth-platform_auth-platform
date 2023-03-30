53f85533d6da8beacf7978e412fce571219b0b59
sakaty <sakata@twogate.com>
2023-03-30 15:21:52 +0000

    add schemas

--- db/schema.rb
+++ db/schema.rb
@@ -16,10 +16,80 @@ ActiveRecord::Schema[7.0].define(version: 0) do
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
-  create_table "tenants", id: :string, force: :cascade do |t|
+  create_table "contact_addresses", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.citext "tenant_id", null: false
+    t.uuid "user_id", null: false
+    t.boolean "is_default"
+    t.string "zip_code"
+    t.integer "prefecture_code"
+    t.string "city"
+    t.string "address1"
+    t.string "address2"
+    t.string "contact_tel"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tenant_id"], name: "index_contact_addresses_on_tenant_id"
+    t.index ["user_id"], name: "index_contact_addresses_on_user_id"
+  end
+
+  create_table "delivary_address", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.citext "tenant_id", null: false
+    t.uuid "user_id", null: false
+    t.boolean "is_default"
+    t.string "zip_code"
+    t.integer "prefecture_code"
+    t.string "city"
+    t.string "address1"
+    t.string "address2"
+    t.string "contact_tel"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tenant_id"], name: "index_delivary_address_on_tenant_id"
+    t.index ["user_id"], name: "index_delivary_address_on_user_id"
+  end
+
+  create_table "tenants", id: :citext, force: :cascade do |t|
     t.string "name"
+    t.string "domain"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "user_profiles", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.citext "tenant_id", null: false
+    t.uuid "user_id", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "first_name_kana"
+    t.string "last_name_kane"
+    t.date "birth_date"
+    t.string "gender"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tenant_id"], name: "index_user_profiles_on_tenant_id"
+    t.index ["user_id"], name: "index_user_profiles_on_user_id"
+  end
+
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.citext "tenant_id", null: false
+    t.string "uid", null: false
+    t.string "email"
+    t.string "encrypted_password"
+    t.string "tel"
+    t.boolean "tel_verified", default: false
+    t.string "email_confirm_code"
+    t.boolean "email_verified", default: false
+    t.string "password_reset_code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tenant_id"], name: "index_users_on_tenant_id"
+  end
+
+  add_foreign_key "contact_addresses", "tenants", name: "fk_contact_addresses_tenants"
+  add_foreign_key "contact_addresses", "users", name: "fk_contact_addresses_users"
+  add_foreign_key "delivary_address", "tenants", name: "fk_delivary_address_tenants"
+  add_foreign_key "delivary_address", "users", name: "fk_delivary_address_users"
+  add_foreign_key "user_profiles", "tenants", name: "fk_user_profiles_tenants"
+  add_foreign_key "user_profiles", "users", name: "fk_user_profiles_users"
+  add_foreign_key "users", "tenants", name: "fk_users_tenants"
 end
--- db/structure.sql
+++ db/structure.sql
@@ -42,15 +42,111 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
+-- Name: contact_addresses; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.contact_addresses (
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    tenant_id public.citext NOT NULL,
+    user_id uuid NOT NULL,
+    is_default boolean,
+    zip_code character varying,
+    prefecture_code integer,
+    city character varying,
+    address1 character varying,
+    address2 character varying,
+    contact_tel character varying,
+    created_at timestamp(6) without time zone NOT NULL,
+    updated_at timestamp(6) without time zone NOT NULL
+);
+
+
+--
+-- Name: delivary_address; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.delivary_address (
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    tenant_id public.citext NOT NULL,
+    user_id uuid NOT NULL,
+    is_default boolean,
+    zip_code character varying,
+    prefecture_code integer,
+    city character varying,
+    address1 character varying,
+    address2 character varying,
+    contact_tel character varying,
+    created_at timestamp(6) without time zone NOT NULL,
+    updated_at timestamp(6) without time zone NOT NULL
+);
+
+
+--
 -- Name: tenants; Type: TABLE; Schema: public; Owner: -
 --
 
 CREATE TABLE public.tenants (
-    id character varying NOT NULL,
+    id public.citext NOT NULL,
     name character varying,
+    domain character varying,
     created_at timestamp(6) without time zone NOT NULL,
     updated_at timestamp(6) without time zone NOT NULL
 );
+
+
+--
+-- Name: user_profiles; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.user_profiles (
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    tenant_id public.citext NOT NULL,
+    user_id uuid NOT NULL,
+    first_name character varying,
+    last_name character varying,
+    first_name_kana character varying,
+    last_name_kane character varying,
+    birth_date date,
+    gender character varying,
+    created_at timestamp(6) without time zone NOT NULL,
+    updated_at timestamp(6) without time zone NOT NULL
+);
+
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.users (
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    tenant_id public.citext NOT NULL,
+    uid character varying NOT NULL,
+    email character varying,
+    encrypted_password character varying,
+    tel character varying,
+    tel_verified boolean DEFAULT false,
+    email_confirm_code character varying,
+    email_verified boolean DEFAULT false,
+    password_reset_code character varying,
+    created_at timestamp(6) without time zone NOT NULL,
+    updated_at timestamp(6) without time zone NOT NULL
+);
+
+
+--
+-- Name: contact_addresses contact_addresses_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.contact_addresses
+    ADD CONSTRAINT contact_addresses_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: delivary_address delivary_address_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.delivary_address
+    ADD CONSTRAINT delivary_address_pkey PRIMARY KEY (id);
 
 
 --
@@ -59,6 +155,127 @@ CREATE TABLE public.tenants (
 
 ALTER TABLE ONLY public.tenants
     ADD CONSTRAINT tenants_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: user_profiles user_profiles_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.user_profiles
+    ADD CONSTRAINT user_profiles_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: index_contact_addresses_on_tenant_id; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_contact_addresses_on_tenant_id ON public.contact_addresses USING btree (tenant_id);
+
+
+--
+-- Name: index_contact_addresses_on_user_id; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_contact_addresses_on_user_id ON public.contact_addresses USING btree (user_id);
+
+
+--
+-- Name: index_delivary_address_on_tenant_id; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_delivary_address_on_tenant_id ON public.delivary_address USING btree (tenant_id);
+
+
+--
+-- Name: index_delivary_address_on_user_id; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_delivary_address_on_user_id ON public.delivary_address USING btree (user_id);
+
+
+--
+-- Name: index_user_profiles_on_tenant_id; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_user_profiles_on_tenant_id ON public.user_profiles USING btree (tenant_id);
+
+
+--
+-- Name: index_user_profiles_on_user_id; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_user_profiles_on_user_id ON public.user_profiles USING btree (user_id);
+
+
+--
+-- Name: index_users_on_tenant_id; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_users_on_tenant_id ON public.users USING btree (tenant_id);
+
+
+--
+-- Name: contact_addresses fk_contact_addresses_tenants; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.contact_addresses
+    ADD CONSTRAINT fk_contact_addresses_tenants FOREIGN KEY (tenant_id) REFERENCES public.tenants(id);
+
+
+--
+-- Name: contact_addresses fk_contact_addresses_users; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.contact_addresses
+    ADD CONSTRAINT fk_contact_addresses_users FOREIGN KEY (user_id) REFERENCES public.users(id);
+
+
+--
+-- Name: delivary_address fk_delivary_address_tenants; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.delivary_address
+    ADD CONSTRAINT fk_delivary_address_tenants FOREIGN KEY (tenant_id) REFERENCES public.tenants(id);
+
+
+--
+-- Name: delivary_address fk_delivary_address_users; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.delivary_address
+    ADD CONSTRAINT fk_delivary_address_users FOREIGN KEY (user_id) REFERENCES public.users(id);
+
+
+--
+-- Name: user_profiles fk_user_profiles_tenants; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.user_profiles
+    ADD CONSTRAINT fk_user_profiles_tenants FOREIGN KEY (tenant_id) REFERENCES public.tenants(id);
+
+
+--
+-- Name: user_profiles fk_user_profiles_users; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.user_profiles
+    ADD CONSTRAINT fk_user_profiles_users FOREIGN KEY (user_id) REFERENCES public.users(id);
+
+
+--
+-- Name: users fk_users_tenants; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT fk_users_tenants FOREIGN KEY (tenant_id) REFERENCES public.tenants(id);
 
 
 --
