79ed10c888007eefb376b2ef59b042661f7dd0fe
ʞ <keiya@twogate.com>
2023-05-29 21:16:36 +0900

    workaround session store

--- config/application.rb
+++ config/application.rb
@@ -58,6 +58,6 @@ module RailsApp
     # --------------------------------------------------------------------------
     config.action_controller.include_all_helpers = false
 
-    config.session_config = { domain: :all, tld_length: 4 } # workaround
+    config.session_store :cookie_store, key: '_rails_app_session', domain: :all, tld_length: 4 # workaround
   end
 end
