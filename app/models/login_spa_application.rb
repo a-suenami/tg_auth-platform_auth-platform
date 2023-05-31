b17dbe3275f4f3bf5908abb07f9179eaa1ec91b7
sakaty <sakata@twogate.com>
2023-05-31 22:10:58 +0000

    不要な関数削除

--- app/models/login_spa_application.rb
+++ app/models/login_spa_application.rb
@@ -4,11 +4,6 @@ class LoginSpaApplication < ApplicationRecord
   extend T::Sig
   include Multitenancy
 
-  sig { params(return_to: T.untyped).returns(T::Boolean) }
-  def vaild_return_to?(return_to)
-    (allowed_logout_urls.split(',') & [return_to]).count.positive?
-  end
-
   sig { returns(String) }
   def login_url_with_flag
     return '' if login_url.blank?
