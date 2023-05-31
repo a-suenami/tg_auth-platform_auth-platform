247c72b58368368797eaed74ba208e9fb82572ca
sakaty <sakata@twogate.com>
2023-05-31 20:54:39 +0000

    ログインspaにredirect時 get queryにフラグ付加

--- app/models/login_spa_application.rb
+++ app/models/login_spa_application.rb
@@ -8,4 +8,14 @@ class LoginSpaApplication < ApplicationRecord
   def vaild_return_to?(return_to)
     (allowed_logout_urls.split(',') & [return_to]).count.positive?
   end
+
+  sig { returns(String) }
+  def login_url_with_flag
+    return '' if login_url.blank?
+
+    uri = URI.parse(T.must(login_url))
+    query = URI.decode_www_form(uri.query || '') << ['in_oauth_flow', 'true']
+    uri.query = URI.encode_www_form(query)
+    uri.to_s
+  end
 end
