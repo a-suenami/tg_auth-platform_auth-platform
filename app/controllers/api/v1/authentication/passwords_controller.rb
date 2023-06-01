b835d780b4a32abb92ad34c02f0e1404e45b7532
sakaty <sakata@twogate.com>
2023-06-01 01:07:55 +0000

    パスワード更新API

--- app/controllers/api/v1/authentication/passwords_controller.rb
+++ app/controllers/api/v1/authentication/passwords_controller.rb
@@ -1,24 +1,33 @@
 module API::V1::Authentication
   class PasswordsController < ApplicationController
-    before_action :registrations_session_authenticate
+    include CookieAuthable
+    skip_before_action :session_authenticate, only: [:create]
+    before_action :registrations_session_authenticate, only: [:create]
 
     def create
-      return handle_400 error_details: ['already created passowrd'] if @user.password_digest.present?
+      return handle_400 error_details: ['already created passowrd'] if @current_user.password_digest.present?
 
-      if @user.update(password_params)
-        session[:current_user_id] = @user.id
+      if @current_user.update(password_params)
+        session[:current_user_id] = @current_user.id
         head :no_content
       else
         handle_400 error_details: ['failed to create password']
       end
     end
 
+    def update
+      if @current_user.update(password_params)
+        head :no_content
+      else
+        handle_400 error_details: ['failed to create password']
+      end
+    end
     private
 
     def registrations_session_authenticate
       return handle_401 error_details: ['session not set'] if session[:registering_user_id].blank?
 
-      @user = User.find session[:registering_user_id]
+      @current_user = User.find session[:registering_user_id]
     end
 
     def password_params
