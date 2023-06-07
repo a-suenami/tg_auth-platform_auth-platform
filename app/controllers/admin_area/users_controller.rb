b88bca7935298480f02aa2f1e4d857ced9eecef6
sakaty <sakata@twogate.com>
2023-06-07 11:22:18 +0000

    管理画面　検索機能

--- app/controllers/admin_area/users_controller.rb
+++ app/controllers/admin_area/users_controller.rb
@@ -2,6 +2,11 @@ module AdminArea
   class UsersController < ApplicationController
     def index
       @users = User.all
+      @users = @users.where(id: params[:id]) if params[:id].present?
+      @users = @users.where(email: params[:email]) if params[:email].present?
+      @email = params[:email]
+      @id = params[:id]
+      @pagy, @users = pagy @users
     end
 
     def show
