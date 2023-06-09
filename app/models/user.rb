5602f3e27be36e99ffd381346e7850ffc5fb7aca
sakaty <sakata@twogate.com>
2023-06-09 07:47:15 +0000

    password バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   extend T::Sig
   include Multitenancy
   has_secure_password validations: false
+  # password validation 英数字大文字小文字記号をそれぞれ1文字以上含む8文字以上
+  PASSWORD_VALIDATION_REGEX = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)(?=.*?[!-\/:-@\[-`{-~])[!-~]{8,100}\z/.freeze
+  validates :password, allow_nil: true, format: { with: PASSWORD_VALIDATION_REGEX }
 
   has_many :access_grants,
     class_name: 'OauthAccessGrant',
