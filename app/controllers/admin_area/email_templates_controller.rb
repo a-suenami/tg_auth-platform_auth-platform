811141890a55d4b5368a93989d672f0f6cca411b
sakaty <sakata@twogate.com>
2023-06-16 10:18:57 +0000

    email tempalte 多言語化対応

--- app/controllers/admin_area/email_templates_controller.rb
+++ app/controllers/admin_area/email_templates_controller.rb
@@ -13,23 +13,24 @@ module AdminArea
       @email_template = EmailTemplate.new
     end
 
+    def edit
+      @email_template = EmailTemplate.find(params[:id])
+    end
+
     def create
       @email_template = EmailTemplate.create(email_template_params)
       if @email_template.persisted?
-        redirect_to admin_area_email_templates_path, notice: 'EmailTemplate was successfully created.'
+        redirect_to admin_area_email_templates_path, notice: t('helpers.messages.created')
       else
         render :new, status: :unprocessable_entity
       end
     end
 
-    def edit
-      @email_template = EmailTemplate.find(params[:id])
-    end
 
     def update
       @email_template = EmailTemplate.find(params[:id])
       if @email_template.update(email_template_params)
-        redirect_to admin_area_email_templates_path, notice: 'EmailTemplate was successfully updated.'
+        redirect_to admin_area_email_templates_path, notice: t('helpers.messages.updated')
       else
         render :edit, status: :unprocessable_entity
       end
@@ -38,10 +39,11 @@ module AdminArea
     def destroy
       email_template = EmailTemplate.find(params[:id])
       email_template.destroy!
-      redirect_to admin_area_email_templates_path, notice: 'EmailTemplate was successfully destroyed.',  status: :see_other
+      redirect_to admin_area_email_templates_path, notice: t('helpers.messages.destroyed'),  status: :see_other
     end
 
     private
+
     def email_template_params
       params.require(:email_template).permit(:template_type, :name, :subject, :body)
     end
