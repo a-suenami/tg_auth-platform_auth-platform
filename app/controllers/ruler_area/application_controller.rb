885659edc4da582f6e1403c8aee19251c1d94c2e
sakaty <sakata@twogate.com>
2023-06-30 11:51:56 +0000

    ruler画面 テナントを選択式に

--- app/controllers/ruler_area/application_controller.rb
+++ app/controllers/ruler_area/application_controller.rb
@@ -5,7 +5,6 @@ module RulerArea
     include Pagy::Backend
 
     before_action :authenticate!
-    before_action :set_tenant
 
     def authenticate!
       redirect_to ruler_area_login_path unless signed_in?
@@ -19,16 +18,6 @@ module RulerArea
       current_ruler.present?
     end
 
-    def root
-      flash[:alert] = 'this is an example message' # rubocop:disable Rails
-      flash[:notice] = 'you can also use notice level flash' # rubocop:disable Rails
-    end
-
-    private
-
-    def set_tenant
-      RequestStore.store[:current_tenant_domain] = request.host || '-'
-      Tenant.current
-    end
+    def root; end
   end
 end
---
+++ app/controllers/ruler_area/tenants/application_controller.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module RulerArea::Tenants
+  class ApplicationController < RulerArea::ApplicationController
+    include Pagy::Backend
+
+    before_action :set_tenant
+
+    def root; end
+
+    private
+
+    def set_tenant
+      RequestStore.store[:current_tenant_domain] = Tenant.find(params[:tenant_id]).domain || '-'
+      @tenant_id = params[:tenant_id]
+      Tenant.current
+    end
+  end
+end
