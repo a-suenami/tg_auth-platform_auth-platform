eec59cb0ea3d7f6b6b40dcfc00d41e4afd104c28
sakaty <sakata@twogate.com>
2023-07-11 09:27:35 +0000

    fix spec

--- app/models/oauth_access_token.rb
+++ app/models/oauth_access_token.rb
@@ -9,11 +9,14 @@ class OauthAccessToken < ApplicationRecord
 
   sig { returns(T::Boolean) }
   def update_linked_application
-    user = User.where(id: self.resource_owner_id)
-    return false if user.empty?
+    return false if self.resource_owner_id.blank?
+    return false unless User.find(self.resource_owner_id).present?
+    return false unless OauthApplication.find(self.application_id).present?
 
     linked_application = Users::LinkedApplication.find_or_initialize_by(tenant_id: self.tenant_id, user_id: self.resource_owner_id, oauth_application_id: self.application_id)
-    linked_application.scopes = (linked_application.scopes.split | self.scopes.to_s.split).join(' ')
+    old_scopes = linked_application.scopes.present? ? linked_application.scopes.split : []
+    new_scopes = self.scopes.present? ? self.scopes.to_s.split : []
+    linked_application.scopes = (old_scopes | new_scopes).join(' ')
     linked_application.last_linked_at = Time.zone.now
     linked_application.save
   end
