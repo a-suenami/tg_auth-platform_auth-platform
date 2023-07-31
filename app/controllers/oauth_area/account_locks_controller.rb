460d69c0ec3b33ef015cb67855caf08a6636c3ce
sakaty <sakata@twogate.com>
2023-07-31 13:01:16 +0000

    アカウントロック　リファクタ & specケース追加

--- app/controllers/oauth_area/account_locks_controller.rb
+++ app/controllers/oauth_area/account_locks_controller.rb
@@ -2,19 +2,11 @@ module OauthArea
   class AccountLocksController < ApplicationController
     # Account Lock解除 エンドポイント
     def unlock
-      if params[:unlock_token].present?
-        account_lock = AccountLock.find_by(unlock_token: params[:unlock_token])
-        if account_lock.present?
-          account_lock.reset_failed_attempts
-          account_lock.save
-
-          @login_url = if session[:auth_url].present?
-            Tenant.current.login_spa_application.login_url_with_flag
-          end
-          render 'unlock'
-        else
-          render 'oauth_area/sessions/error'
+      if AccountLocks::UnlockByTokenService.new.execute(token: params[:unlock_token])
+        @login_url = if session[:auth_url].present?
+          Tenant.current.login_spa_application.login_url_with_flag
         end
+        render 'unlock'
       else
         render 'oauth_area/sessions/error'
       end
--- app/services/users/email_change_service.rb
+++ app/services/users/email_change_service.rb
@@ -21,6 +21,10 @@ module Users
         user.save!
         email_verifier.used_at = Time.zone.now
         email_verifier.save!
+        # アカウントロックはemailを元にカウントするので、email変更時削除
+        if user.account_lock.present?
+          user.account_lock.destroy
+        end
       else
         raise Exceptions::Services::Users::ExpiredEmailVerificationCode
       end
