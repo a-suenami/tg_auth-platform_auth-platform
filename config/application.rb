a112156554311a99fc79b5246b984a167b25e4d1
sakaty <sakata@twogate.com>
2023-08-04 12:34:32 +0000

    domain: :all削除
    動的に設定できるならいらない

--- config/application.rb
+++ config/application.rb
@@ -65,6 +65,6 @@ module RailsApp
     # --------------------------------------------------------------------------
     config.action_controller.include_all_helpers = false
 
-    config.session_store :original_cookie_store, key: '_rails_app_session', domain: :all
+    config.session_store :original_cookie_store, key: '_rails_app_session'
   end
 end
