c7f58e839ab942d0938054fcc89bcd5378fa4b0b
sakaty <sakata@twogate.com>
2023-10-11 12:24:33 +0000

    fix PublishService

--- app/services/publish_events/publish_service.rb
+++ app/services/publish_events/publish_service.rb
@@ -3,17 +3,21 @@
 module PublishEvents
   class PublishService < ::BaseService
     def execute(user:, action_code:)
+      return false if Settings&.aws&.region.blank?
+      return false if Settings&.aws&.access_key_id.blank?
+      return false if Settings&.aws&.secret_access_key.blank?
+      return false if Settings&.aws&.event_bus_name.blank?
+
       client = ::Aws::EventBridge::Client.new(
         region: Settings.aws.region,
         credentials: ::Aws::Credentials.new(Settings.aws.access_key_id, Settings.aws.secret_access_key),
-        # ...
       )
 
       client.put_events({
         entries: [ # required
           {
-            source: 'String',
-            detail_type: 'String',
+            source: 'id-platform.twogate',
+            detail_type: 'Id Platform User',
             detail: {
               tenant_id: user.tenant_id,
               user_id: user.id,
