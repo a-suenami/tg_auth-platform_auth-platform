02f4dc183761ecad0b2da6094d3e5cef4c5dfbd6
sakaty <sakata@twogate.com>
2023-10-20 11:27:50 +0000

    cookie secure属性設定
    production環境ではデフォルトでtrueっぽいから、あんまり意味ないけど

--- config/application.rb
+++ config/application.rb
@@ -65,6 +65,6 @@ module RailsApp
     # --------------------------------------------------------------------------
     config.action_controller.include_all_helpers = false
 
-    config.session_store :original_cookie_store, key: '_rails_app_session'
+    config.session_store :original_cookie_store, key: '_rails_app_session', secure: Rails.env.production?
   end
 end
