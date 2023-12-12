8657dd6e1ea97f79dacd79640264e2a699c67933
sakaty <sakata@twogate.com>
2023-12-12 19:30:07 +0900

    fix srb tc

--- app/services/authentication/send_verification_sms_service.rb
+++ app/services/authentication/send_verification_sms_service.rb
@@ -43,13 +43,13 @@ module Authentication
       return if Settings.sms.disable_rate_limit
 
       # 同一電話番号 5件/3hours
-      raise Exceptions::Authentication::SmsSendLimit if Users::SmsVerifier.where(created_at: 3.hours.ago..).where(phone_number:).count >= 5
+      raise Exceptions::Authentication::SmsSendLimit if Users::SmsVerifier.where('created_at > ?', 3.hours.ago).where(phone_number:).count >= 5
       # 同一電話番号 10件/24hours
-      raise Exceptions::Authentication::SmsSendLimit if Users::SmsVerifier.where(created_at: 24.hours.ago..).where(phone_number:).count >= 10
+      raise Exceptions::Authentication::SmsSendLimit if Users::SmsVerifier.where('created_at > ?', 24.hours.ago).where(phone_number:).count >= 10
       # 同一IP      100件/1hours
-      raise Exceptions::Authentication::SmsSendLimit if Users::SmsVerifier.where(created_at: 1.hour.ago..).where(ip_address:).count >= 100
+      raise Exceptions::Authentication::SmsSendLimit if Users::SmsVerifier.where('created_at > ?', 1.hour.ago).where(ip_address:).count >= 100
       # 同一ユーザ   10件/24hours
-      raise Exceptions::Authentication::SmsSendLimit if Users::SmsVerifier.where(created_at: 24.hours.ago..).where(user:).count >= 10
+      raise Exceptions::Authentication::SmsSendLimit if Users::SmsVerifier.where('created_at > ?', 24.hours.ago).where(user:).count >= 10
     end
   end
 end
