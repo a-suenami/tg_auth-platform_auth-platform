e240d932df8f87d4c392f86904617d20661e7703
sakaty <sakata@twogate.com>
2023-12-29 16:42:09 +0900

    fix rubocop

--- app/models/concerns/address_utilisable.rb
+++ app/models/concerns/address_utilisable.rb
@@ -18,7 +18,8 @@ module AddressUtilisable
     T.bind(self, T.class_of(ApplicationRecord))
     # 日本住所の場合のみ、バリデーションを行う
     with_options if: :domestic_address? do
-      validates :zip_code, format: { with: /\A\d{3}-?\d{4}\z/, message: 'は不正な値です' }, presence: true
+      validates :zip_code, presence: true
+      validates :zip_code, format: { with: /\A\d{3}-?\d{4}\z/ }, allow_blank: true
       validates :prefecture_code, presence: true
       validates :city, presence: true
       validates :street, presence: true
