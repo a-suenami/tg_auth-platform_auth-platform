a734764848bf4c93435ae80794699416b1f52c4f
sakaty <sakata@twogate.com>
2024-01-12 21:48:44 +0900

    Users::UpdateService 例外を吐くように

--- app/services/users/update_service.rb
+++ app/services/users/update_service.rb
@@ -3,7 +3,7 @@
 module Users
   class UpdateService < BaseService
     def execute(user:)
-      user.update(params)
+      user.update!(params)
       # zipcodeはハイフンを削除して保存する
       user&.contact_address&.zip_code = user&.contact_address&.zip_code&.delete('-') if user&.contact_address&.zip_code.present?
       user.save!
