c24e31ba9f00c7abc92349053ce666de0e3a7dba
keiya <keiya_21@yahoo.co.jp>
2024-01-25 17:44:36 +0900

    rubocop

--- app/services/authentication/verify_sms_service.rb
+++ app/services/authentication/verify_sms_service.rb
@@ -11,7 +11,7 @@ module Authentication
 
       if !Rails.env.production? && Settings.super_mode == true # SUPER_MODE では常に成功
         user.sms_verified = true
-        user.phone_number = "+81#{rand(7..9)}0#{rand(70000000..90000000)}"
+        user.phone_number = "+81#{rand(7..9)}0#{rand(70_000_000..90_000_000)}"
         user.save!(validate: false)
         return user
       end
