e281a1f13eb74b8e752248776cfea156b754d974
sakaty <sakata@twogate.com>
2024-01-26 20:59:55 +0900

    ユーザ削除時 AccountLockも削除

--- app/services/users/destroy_service.rb
+++ app/services/users/destroy_service.rb
@@ -4,6 +4,10 @@ module Users
   class DestroyService < BaseService
     def execute(user:)
       user.update!(deleted: true)
+      # 削除済みユーザのAccountLockが残留しないように削除
+      if user.account_lock.present?
+        user.account_lock.destroy!
+      end
       # aws event bridgeにイベント発行
       PublishEvents::PublishService.new.execute(user:, action_code: :delete)
     end
