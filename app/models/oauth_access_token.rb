dd98d77e5a5dc967554ba2efaab4ab299218d6f6
sakaty <sakata@twogate.com>
2024-12-23 17:57:07 +0900

    linked application password credentials grantの場合は無視

--- app/models/oauth_access_token.rb
+++ app/models/oauth_access_token.rb
@@ -11,6 +11,9 @@ class OauthAccessToken < ApplicationRecord
 
   sig { returns(T::Boolean) }
   def update_linked_application
+    # password credentials grantの場合は無視
+    return false if self.resource_owner_id.nil?
+
     Users::LinkedApplications::UpdateService.new.execute(tenant_id: self.tenant_id, resource_owner_id: self.resource_owner_id, oauth_application_id: self.application_id,
 scopes: T.let(self.scopes, T.nilable(Doorkeeper::OAuth::Scopes)),)
   end
