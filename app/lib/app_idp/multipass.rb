649f35990d8724f86a33f894ab32fb3db41dd5f6
sakaty <sakata@twogate.com>
2025-05-22 22:35:16 +0900

    SafeUrlParser 削除

--- app/lib/app_idp/multipass.rb
+++ app/lib/app_idp/multipass.rb
@@ -23,9 +23,46 @@ module AppIdp
 
     private
 
+    # Parses a URL string into a URI object, handling various encoding patterns
+    # @param url [String] The URL to parse
+    # @return [URI::Generic] A parsed URI object
+    # @raise [URI::InvalidURIError] If the URL is fundamentally invalid
+    def safe_parse_url(url)
+      return nil if url.blank?
+
+      # 1. https://が%エンコードされている場合はデコード
+      decoded_url = decode_encoded_scheme(url)
+      # 2. 非ASCII文字が含まれている場合は%エンコード
+      encoded_url = encode_non_ascii(decoded_url)
+      # 3. パースして返す
+      URI.parse(encoded_url)
+    end
+
+    # %エンコードされたhttps://をデコードする
+    # @param url [String] デコードするURL
+    # @return [String] デコードされたURL
+    def decode_encoded_scheme(url)
+      return url unless url.include?('%')
+
+      if url.match?(/https?%3A%2F%2F/)
+        URI::DEFAULT_PARSER.unescape(url)
+      else
+        url
+      end
+    end
+
+    # 非ASCII文字をエンコードする
+    # @param url [String] エンコードするURL
+    # @return [String] エンコードされたURL
+    def encode_non_ascii(url)
+      return url if url.ascii_only?
+
+      URI::DEFAULT_PARSER.escape(url)
+    end
+
     # return_toのホストチェック
     def validate_return_to(return_to, multipass_store)
-      uri = SafeUrlParser.parse(return_to)
+      uri = safe_parse_url(return_to)
 
       if !uri.host
         return_to
