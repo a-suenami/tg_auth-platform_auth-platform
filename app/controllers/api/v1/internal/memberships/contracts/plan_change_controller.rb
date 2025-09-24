2623329f8b3b3fa66128a165b102f481a77319d8
sakaty <sakata@twogate.com>
2025-09-24 18:41:24 +0900

    fix srb

--- app/controllers/api/v1/internal/memberships/contracts/plan_change_controller.rb
+++ app/controllers/api/v1/internal/memberships/contracts/plan_change_controller.rb
@@ -8,28 +8,6 @@ module API::V1::Internal::Memberships::Contracts
 
     # TODO: 料金の確認
     def preview
-      # プラン変更のプレビュー情報を返す
-      new_membership_plan = Memberships::Plan.find(plan_change_params[:memberships_plan_id])
-
-      # 変更可能かチェック
-      service = UserStripe::ChangePlanService.new
-      can_change = service.send(:validate_plan_change, contract: @contract, new_membership_plan:)
-
-      render json: {
-        can_change:,
-        current_plan: {
-          id: @contract.current_billing_profile&.membership_plan&.id,
-          name: @contract.current_billing_profile&.membership_plan&.name,
-          amount: @contract.current_billing_profile&.membership_plan&.amount,
-          validity_period: @contract.current_billing_profile&.membership_plan&.validity_period,
-        },
-        new_plan: {
-          id: new_membership_plan.id,
-          name: new_membership_plan.name,
-          amount: new_membership_plan.amount,
-          validity_period: new_membership_plan.validity_period,
-        },
-      }
     end
 
     def create
