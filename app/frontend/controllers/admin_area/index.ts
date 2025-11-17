c2492cf954a35e3f970e109cc7dc813a1003a12c
Ryan Vo <thuan.vo@executionlab.asia>
2025-11-17 15:44:08 +0700

    feat: Implement user auto tagging form with dynamic rule blocks and conditions

--- app/frontend/controllers/admin_area/index.ts
+++ app/frontend/controllers/admin_area/index.ts
@@ -1,6 +1,8 @@
 import { application } from '@app/controllers/application';
 import ScheduleFormController from './schedule_form_controller';
+import UserAutoTaggingFormController from './user_auto_tagging_form_controller';
 
 application.register('schedule-form', ScheduleFormController);
+application.register('user-auto-tagging-form', UserAutoTaggingFormController);
 
 console.log('this is frontend/controllers/admin_area/index.ts')
