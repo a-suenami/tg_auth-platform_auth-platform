3bc8904804f03f27343fd45dc9547c3afb904799
Ryoji Mizoguchi <mizoguchi@twogate.com>
2025-11-20 17:20:35 +0900

    請求先住所の作成ページの対応

--- app/controllers/admin_area/billing_addresses_controller.rb
+++ app/controllers/admin_area/billing_addresses_controller.rb
@@ -2,6 +2,10 @@
 
 module AdminArea
   class BillingAddressesController < AdminArea::ApplicationController
+    def new
+      @user = User.find(params[:user_id])
+    end
+
     def edit
       @user = User.find(params[:user_id])
     end
