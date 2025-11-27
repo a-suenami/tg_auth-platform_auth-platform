41ffd49b3e51d90e3d373fe2db062d4db9783fff
Ryan Vo <thuan.vo@executionlab.asia>
2025-11-27 15:48:31 +0700

    Refactor auto user tag rules with config validation and error handling

--- app/models/user_auto_tagging/rule.rb
+++ app/models/user_auto_tagging/rule.rb
@@ -24,96 +24,21 @@ class UserAutoTagging::Rule < ApplicationRecord
 
   scope :ordered, -> { order(position: :asc) }
 
+  # TODO: Rule execution
+  # def to_rule
+  #   Build rule engine instance for execution
+  #   UserTagRules::RuleFactory.build(condition_type, config)
+  # end
+
   private
 
   sig { void }
   def config_matches_condition_type
     return if config.blank? || condition_type.blank?
 
-    case condition_type
-    when 'membership', 'plan'
-      validate_subscription_config
-    when 'prefecture', 'gender'
-      validate_values_array
-    when 'age'
-      validate_age_range
-    when 'account_link'
-      validate_single_value
-    end
-  end
-
-  sig { void }
-  def validate_subscription_config
-    subscription_type = config['subscription_type']
-    values = config['values']
-
-    unless %w[current duration].include?(subscription_type)
-      errors.add(:config, 'subscription_type must be "current" or "duration"')
-    end
-
-    unless values.is_a?(Array) && values.any?
-      errors.add(:config, 'values must be a non-empty array')
-    end
-
-    if subscription_type == 'duration'
-      duration_value = config['duration_value']
-      duration_unit = config['duration_unit']
-
-      unless duration_value.is_a?(Integer) && duration_value.positive?
-        errors.add(:config, 'duration_value must be a positive integer')
-      end
-
-      unless %w[days months].include?(duration_unit)
-        errors.add(:config, 'duration_unit must be "days" or "months"')
-      end
-    end
-  end
-
-  sig { void }
-  def validate_values_array
-    values = config['values']
-
-    unless values.is_a?(Array) && values.any?
-      errors.add(:config, 'values must be a non-empty array')
-    end
-
-    if (condition_type == 'gender') && !values.all? { |v| %w[male female].include?(v) }
-      errors.add(:config, 'gender values must be "male" or "female"')
-    end
-  end
-
-  sig { void }
-  def validate_age_range
-    min_age = config['min']
-    max_age = config['max']
-
-    if min_age.present? && (!min_age.is_a?(Integer) || min_age.negative?)
-      errors.add(:config, 'min must be a non-negative integer')
-    end
-
-    if max_age.present? && (!max_age.is_a?(Integer) || max_age.negative?)
-      errors.add(:config, 'max must be a non-negative integer')
-    end
-
-    if min_age.present? && max_age.present? && min_age >= max_age
-      errors.add(:config, 'max must be greater than min')
-    end
-
-    if min_age.blank? && max_age.blank?
-      errors.add(:config, 'at least one of min or max must be present')
-    end
-  end
-
-  sig { void }
-  def validate_single_value
-    value = config['value']
-
-    unless value.present? && value.is_a?(String)
-      errors.add(:config, 'value must be a non-empty string')
-    end
-
-    unless value == 'LINE'
-      errors.add(:config, 'value must be "LINE"')
+    validation_errors = UserTagRules::RuleFactory.validate(condition_type, config)
+    validation_errors.each do |error_message|
+      errors.add(:config, error_message)
     end
   end
 end
---
+++ app/models/user_tag_rules/membership_rule.rb
@@ -0,0 +1,85 @@
+# typed: strict
+# frozen_string_literal: true
+
+module UserTagRules
+  # Rule to check membership subscription (current or duration)
+  #
+  # Config format for current subscription:
+  #   {
+  #     "subscription_type": "current",
+  #     "values": ["1", "2", "3"]  # Membership IDs
+  #   }
+  #
+  # Config format for duration subscription:
+  #   {
+  #     "subscription_type": "duration",
+  #     "values": ["1", "2"],
+  #     "duration_value": 6,
+  #     "duration_unit": "months"
+  #   }
+  #
+  class MembershipRule < AbstractRule
+    extend T::Sig
+
+    sig do
+      params(
+        membership_ids: T::Array[String],
+        subscription_type: String,
+        duration_value: T.nilable(Integer),
+        duration_unit: T.nilable(String)
+      ).void
+    end
+    def initialize(membership_ids:, subscription_type:, duration_value: nil, duration_unit: nil)
+      @membership_ids = T.let(membership_ids, T::Array[String])
+      @subscription_type = T.let(subscription_type, String)
+      @duration_value = T.let(duration_value, T.nilable(Integer))
+      @duration_unit = T.let(duration_unit, T.nilable(String))
+    end
+
+    # TODO: Execution methods
+    # - events(): [:membership_joined, :membership_left] (current)
+    #            [:membership_joined, :membership_left, :new_day_arrived] (duration)
+    # - apply(relation): Filter users based on subscription_type
+
+    # Validate config format
+    #
+    # @param config [Hash] Configuration hash
+    # @return [Array<String>] Array of error messages (empty if valid)
+    sig { params(config: T::Hash[String, T.untyped]).returns(T::Array[String]) }
+    def self.validate_config(config)
+      errors = []
+
+      subscription_type = config['subscription_type']
+      unless %w[current duration].include?(subscription_type)
+        errors << I18n.t('user_tag_rules.errors.subscription_type_invalid')
+        return errors
+      end
+
+      values = config['values']
+      unless values.is_a?(Array) && values.any?
+        errors << I18n.t('user_tag_rules.errors.values_empty')
+      end
+
+      if values.is_a?(Array) && !values.all? { |v| v.is_a?(String) && v.present? }
+        errors << I18n.t('user_tag_rules.errors.membership_ids_invalid')
+      end
+
+      # Validate duration-specific fields
+      if subscription_type == 'duration'
+        duration_value = config['duration_value']
+        unless duration_value.present? && duration_value.to_i.positive?
+          errors << I18n.t('user_tag_rules.errors.duration_value_invalid')
+        end
+
+        duration_unit = config['duration_unit']
+        unless %w[days months years].include?(duration_unit)
+          errors << I18n.t('user_tag_rules.errors.duration_unit_invalid')
+        end
+      end
+
+      errors
+    end
+
+    # TODO: Build rule from config
+  end
+end
