64582220817df04fb0347344213419f6cc92df4c
Ryan Vo <thuan.vo@executionlab.asia>
2025-12-01 10:52:01 +0700

    Implement auto-tagging UI: add tag selector controller and integrate into forms

--- app/frontend/controllers/admin_area/index.ts
+++ app/frontend/controllers/admin_area/index.ts
@@ -1,8 +1,10 @@
 import { application } from '@app/controllers/application';
 import ScheduleFormController from './schedule_form_controller';
 import UserAutoTaggingFormController from './user_auto_tagging_form_controller';
+import AutoTaggingTagSelectorController from "./auto_tagging_tag_selector_controller";
 
 application.register('schedule-form', ScheduleFormController);
 application.register('user-auto-tagging-form', UserAutoTaggingFormController);
+application.register('auto-tagging-tag-selector', AutoTaggingTagSelectorController);
 
 console.log('this is frontend/controllers/admin_area/index.ts')
