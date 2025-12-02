42311cc5552ec0d44481ccef68c376791a101964
Ryan Vo <thuan.vo@executionlab.asia>
2025-12-02 14:50:10 +0700

    Implement unique tag constraint per tenant for auto-tagging rules and update related error messages

--- app/controllers/admin_area/user_auto_taggings_controller.rb
+++ app/controllers/admin_area/user_auto_taggings_controller.rb
@@ -12,11 +12,11 @@ module AdminArea
     def new
       @user_auto_tagging = UserAutoTagging.new
       @user_auto_tagging.build_schedule
-      @user_tags = UserTag.ordered
+      @user_tags = available_tags_for_selection
     end
 
     def edit
-      @user_tags = UserTag.ordered
+      @user_tags = available_tags_for_selection(exclude_rule: @user_auto_tagging)
     end
 
     def create
@@ -26,6 +26,7 @@ module AdminArea
       if @user_auto_tagging.save
         redirect_to admin_area_user_auto_taggings_path, notice: 'オートタグ設定を保存しました'
       else
+        @user_tags = available_tags_for_selection
         render :new, status: :unprocessable_entity
       end
     end
@@ -36,6 +37,7 @@ module AdminArea
       if @user_auto_tagging.update(user_auto_tagging_params)
         redirect_to admin_area_user_auto_taggings_path, notice: 'オートタグ設定を更新しました'
       else
+        @user_tags = available_tags_for_selection(exclude_rule: @user_auto_tagging)
         render :edit, status: :unprocessable_entity
       end
     end
@@ -49,6 +51,19 @@ module AdminArea
 
     def set_user_auto_tagging
       @user_auto_tagging = UserAutoTagging.find(params[:id])
+    end
+
+    # Get tags available for selection (excluding tags already used by other rules)
+    def available_tags_for_selection(exclude_rule: nil)
+      used_tag_ids = UserAutoTaggingTag.pluck(:user_tag_id)
+
+      # If editing, allow tags that are already assigned to this rule
+      if exclude_rule
+        current_rule_tag_ids = exclude_rule.user_tag_ids
+        used_tag_ids -= current_rule_tag_ids
+      end
+
+      UserTag.where.not(id: used_tag_ids).ordered
     end
 
     def user_auto_tagging_params
