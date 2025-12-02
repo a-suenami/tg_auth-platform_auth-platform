8bc9a0e3df7ba5a93af9c706d6ef28ff85375779
Ryan Vo <thuan.vo@executionlab.asia>
2025-12-02 14:33:13 +0700

    Implement unique tag constraint per tenant for auto-tagging rules and update related error messages

--- app/models/user_auto_tagging_tag.rb
+++ app/models/user_auto_tagging_tag.rb
@@ -8,5 +8,6 @@ class UserAutoTaggingTag < ApplicationRecord
   belongs_to :user_auto_tagging
   belongs_to :user_tag
 
-  validates :user_tag_id, uniqueness: { scope: :user_auto_tagging_id }
+  # Each tag can only be used in ONE auto-tagging rule per tenant
+  validates :user_tag_id, uniqueness: { scope: :tenant_id, message: :already_used_in_another_rule }
 end
